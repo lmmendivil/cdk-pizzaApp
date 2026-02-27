a7aaf0ba17442853535e3f609466616d5dff40e9
lmmendivil <159588430+lmmendivil@users.noreply.github.com>
2026-02-27 11:17:52 -0700

    Added newOrder function infra and code + get api

--- lib/cdk-pizza_app-stack.ts
+++ lib/cdk-pizza_app-stack.ts
@@ -8,17 +8,32 @@ export class CdkPizzaAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+
+//FUNCTIONS 
     const newOrderFunction = new Function(this, 'NewOrderFuncion', {
       runtime: Runtime.NODEJS_22_X,
       handler: 'handler.newOrder',
       code: Code.fromAsset('lib/functions'),
 });
 
+    const getOrderFunction = new Function(this, 'GetOrderFuncion', {
+      runtime: Runtime.NODEJS_22_X,
+      handler: 'handler.getOrder',
+      code: Code.fromAsset('lib/functions'),
+});
+
+//API GATEWAY
     const api = new apigateway.RestApi(this, 'PizzeriaApi', {
       restApiName: 'Pizzeria CDK Service',
       });
 
     const orderResource = api.root.addResource('order');
     orderResource.addMethod('POST', new apigateway.LambdaIntegration(newOrderFunction));  
+    orderResource.addResource('{orderId}').addMethod('GET', new apigateway.LambdaIntegration(getOrderFunction));
+  
+
+ 
+
+
   }
 }
