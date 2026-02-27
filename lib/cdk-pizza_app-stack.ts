a89bc9c8f688ffccc51a3236e3b2db2f6dff5169
lmmendivil <159588430+lmmendivil@users.noreply.github.com>
2026-02-27 12:16:26 -0700

    Created SQS pendingOrderQueue and LF prepOrder
    pOQ isn't linked yet to newOrder.

--- lib/cdk-pizza_app-stack.ts
+++ lib/cdk-pizza_app-stack.ts
@@ -2,12 +2,16 @@ import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 
 export class CdkPizzaAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+//SQS
+    const pendingOrdersQueue = new Queue(this, 'PendingOrdersQueue', {});
 
 //FUNCTIONS 
     const newOrderFunction = new Function(this, 'NewOrderFuncion', {
@@ -18,9 +22,24 @@ export class CdkPizzaAppStack extends cdk.Stack {
 
     const getOrderFunction = new Function(this, 'GetOrderFuncion', {
       runtime: Runtime.NODEJS_22_X,
-      handler: 'handler.getOrder',
+      handler: 'handler.GetOrder',
       code: Code.fromAsset('lib/functions'),
 });
+
+    const prepOrderFunction = new Function(this, 'PrepOrderFuncion', {
+      runtime: Runtime.NODEJS_22_X,
+      handler: 'handler.prepOrder',
+      code: Code.fromAsset('lib/functions'),  
+});
+
+    prepOrderFunction.addEventSource(new SqsEventSource(pendingOrdersQueue, {
+      batchSize: 1
+    }));
+    
+
+
+
+
 
 //API GATEWAY
     const api = new apigateway.RestApi(this, 'PizzeriaApi', {
